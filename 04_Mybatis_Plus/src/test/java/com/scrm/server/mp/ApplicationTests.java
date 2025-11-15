af8acc77a41169e029a6c578cda5df169cd6f5c6
hewenyu <he875027547@126.com>
2025-11-15 16:51:14 +0800

    创建 mp module

---
+++ 04_Mybatis_Plus/src/test/java/com/scrm/server/mp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrm.server.mp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
